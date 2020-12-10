9b097ce701350f80673d31ca4d94790d9ebc72fa
Zarko Milosev <z.milosev@gmail.com>
2020-12-10 15:00:31 +0100

    az pipeline update

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "azurerm" {
     features {}
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name = "storageResourceGroup"
+    storage_account_name = "terrastorageacc"
+    container_name = "tfstate"
+    key = "terraform.tfstate"
+  }
+}
+
 resource "azurerm_resource_group" "tf_test" {
   name = "tfmainrg"
   location = "West Europe"
